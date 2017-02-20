f22f6183d6e865a555ecac2f1e91c67621657982
Flatiron School <github email address>
2017-02-20 15:19:44 -0500

    more job data

--- src/dashboard/data.js
+++ src/dashboard/data.js
@@ -1,26 +1,21 @@
 const data = [{
-    "companyName": "Success Academy Charter Schools",
+    "companyName": "Shutterstock",
     "positionTitle": "Cloud Engineer",
-    "companySite": "http://www.successacademies.org",
+    "companySite": "http://www.shutterstock.com",
     "location": "New York City Metro Area",
     "level": "Entry Level",
-    "jobDescription": "At Success Academy, we are redefining what’s possible in public education. Since opening our first school in Harlem in 2006, we have swiftly grown to 41 high-performing schools, from Bed-Stuy to the Bronx, serving approximately 14,000 kids. We are scaling to 100 great schools serving 50,000 children across New York City, bringing transformational change here and across the country. We believe all children, regardless of zip code or family income, deserve the life-changing opportunities that a world-class education brings.\nOur Data Science and Information Systems (DSIS) department ensures we all work collaboratively and more efficiently to support our growing network of schools. By implementing cutting edge technology, built in-house and from 3rd party vendors, we are able to help the organization scale by streamlining processes and making sure everyone has access to mission-critical information 24/7. In effect, the DSIS department's goal is to help everyone at Success work better, more efficiently, and with access to all available sources of information. We are looking for a tactical, organized, and detail-oriented Cloud Engineer to help manage initiatives for our Data Science team.\nOur Cloud Engineer will be responsible for building and maintaining our cloud infrastructure in Amazon Web Services (AWS), while also working with the rest of the Data Science team to build real products that have an immediate effect on our organization and scholars. Reporting directly to our Managing Director, s/he will be comfortable jumping in the deep end and learning new skills, and should be someone who can...",
+    "jobDescription": "As a leader in the stock photo industry, Shutterstock is looking for a highly motivated and talented individual to join our Base engineering team. We strongly believe that in order to put our customers first, we must work closely with them to not only understand their needs but architect solutions that improve their day to day flow. Our core belief is that in order for Shutterstock to succeed, teams and individual team members must be empowered to work with each other to develop the solution that best meets the needs of everyone involved.",
     "requirements": [
-        "Ability to effectively construct SQL queries (mostly Postgres, but some MS SQL Server and MySQL as well)",
-        "High level of comfort with Python",
-        "Experience profiling and optimizing SQL queries"
+      "3+ years of relevant software development experience using JavaScript, HTML and CSS.",
+      "Experience with React.js and writing vanilla JavaScript, preferably ES6.",
+      "Experience with Node.js, working knowledge of the key libraries."
     ],
     "responsibilities": [
-        "Design, build, and maintain infrastructure on AWS environment",
-        "Migrate existing services from physical data centers to AWS cloud",
-        "Write scripts to automate operations and server management",
-        "Maintain QA and Production configuration using automation tools",
-        "Build and maintain operations tools for monitoring, notifications, trending, and analysis",
-        "Define, create, test, and execute operations procedures",
-        "Troubleshoot production issues",
-        "Work comfortably with automation/orchestration software (Ansible)",
-        "Thrive in a fast-paced, collaborative environment with capacity to work with multiple teams on completing a variety of projects",
-        "Bring a strong interest in education reform and working for a mission-driven organization"
+      "Focus on improving our front end discovery experience for Search.",
+      "Contributing to all aspects of the services and applications under the team’s ownership, which includes design, implementation, refactoring, automated testing, deployment, and uptime of the services.",
+      "Helping other team members grow as engineers through code review, pairing, and mentoring.",
+      "Play a major role in the development of key applications and services within the Shutterstock and Content ecosystem.",
+      "Collaboration with others to develop best practices."
     ],
     "perks": [
         "Competitive salary",
@@ -28,11 +23,11 @@ const data = [{
         "Working with teams around the globe and occasionally traveling for educational and work purposes"
     ],
     "isOutOfNetwork": true,
-    "applicationLink": "https://jobs.lever.co/successacademy/f0fe9c2e-47dd-444f-a458-25a479b09741/apply?lever-source=themuse"
+    "applicationLink": "https://www.shutterstock.com/jobs/listings?gh_jid=597168&gh_src=qhno361"
   },
 
   {
-      "companyName": "Amazon",
+      "companyName": "Success Academy Charter School",
       "positionTitle": "Cloud Engineer",
       "companySite": "http://www.successacademies.org",
       "location": "New York City Metro Area",
@@ -66,28 +61,24 @@ const data = [{
 
 
     {
-        "companyName": "Apple",
+        "companyName": "WayUp",
         "positionTitle": "Cloud Engineer",
-        "companySite": "http://www.successacademies.org",
+        "companySite": "http://www.wayup.com",
         "location": "New York City Metro Area",
         "level": "Entry Level",
-        "jobDescription": "At Success Academy, we are redefining what’s possible in public education. Since opening our first school in Harlem in 2006, we have swiftly grown to 41 high-performing schools, from Bed-Stuy to the Bronx, serving approximately 14,000 kids. We are scaling to 100 great schools serving 50,000 children across New York City, bringing transformational change here and across the country. We believe all children, regardless of zip code or family income, deserve the life-changing opportunities that a world-class education brings.\nOur Data Science and Information Systems (DSIS) department ensures we all work collaboratively and more efficiently to support our growing network of schools. By implementing cutting edge technology, built in-house and from 3rd party vendors, we are able to help the organization scale by streamlining processes and making sure everyone has access to mission-critical information 24/7. In effect, the DSIS department's goal is to help everyone at Success work better, more efficiently, and with access to all available sources of information. We are looking for a tactical, organized, and detail-oriented Cloud Engineer to help manage initiatives for our Data Science team.\nOur Cloud Engineer will be responsible for building and maintaining our cloud infrastructure in Amazon Web Services (AWS), while also working with the rest of the Data Science team to build real products that have an immediate effect on our organization and scholars. Reporting directly to our Managing Director, s/he will be comfortable jumping in the deep end and learning new skills, and should be someone who can...",
+        "jobDescription": "Hire has partnered with WayUp New York and, we're looking for an experienced backend developer to join us to help create new features and manage our infrastructure. As we continue to expand and add new products, you will completely own your product and be empowered to make the major architectural decisions that go with releasing any product.Hire has partnered with WayUp New York and, we're looking for an experienced backend developer to join us to help create new features and manage our infrastructure. As we continue to expand and add new products, you will completely own your product and be empowered to make the major architectural decisions that go with releasing any product.",
         "requirements": [
-            "Ability to effectively construct SQL queries (mostly Postgres, but some MS SQL Server and MySQL as well)",
-            "High level of comfort with Python",
-            "Experience profiling and optimizing SQL queries"
+          "Bachelors and/or Masters in Computer Science/Engineering OR related field",
+          "4+ years in software development or related field",
+          "Experience working on backends that are scalable, fault tolerant, well documented and most importantly testable",
+          "Experience with server monitoring/logging and triage tools"
         ],
         "responsibilities": [
-            "Design, build, and maintain infrastructure on AWS environment",
-            "Migrate existing services from physical data centers to AWS cloud",
-            "Write scripts to automate operations and server management",
-            "Maintain QA and Production configuration using automation tools",
-            "Build and maintain operations tools for monitoring, notifications, trending, and analysis",
-            "Define, create, test, and execute operations procedures",
-            "Troubleshoot production issues",
-            "Work comfortably with automation/orchestration software (Ansible)",
-            "Thrive in a fast-paced, collaborative environment with capacity to work with multiple teams on completing a variety of projects",
-            "Bring a strong interest in education reform and working for a mission-driven organization"
+          "Leading the design and development of an end to end a product for launch later this year",
+          "Collaborating with the team members on API development",
+          "Addressing our growing need for scalable architectures, systems and patterns",
+          "Making strategic infrastructure decisions to prepare the site for scale",
+          "Championing and spearheading our technical standards throughout our team"
         ],
         "perks": [
             "Competitive salary",
@@ -95,7 +86,7 @@ const data = [{
             "Working with teams around the globe and occasionally traveling for educational and work purposes"
         ],
         "isOutOfNetwork": true,
-        "applicationLink": "https://jobs.lever.co/successacademy/f0fe9c2e-47dd-444f-a458-25a479b09741/apply?lever-source=themuse"
+        "applicationLink": "https://www.indeed.com/cmp/Wayup?from=vjcmpinfo&fromjk=c09aa53e686ecb53&attributionid=vjcmpinfo"
       }
 
 ]
